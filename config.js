88e6f733af626de697db825cd5aff28279cdd1fd
ashtmz <stskmt@gmail.com>
2026-04-04 14:29:28 +0900

    Configure Apps Script web app URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   sharedStore: {
     mode: "apps-script",
-    webAppUrl: "",
+    webAppUrl: "https://script.google.com/macros/s/AKfycbweAvJbTPf00Gv9TYOVgJWyHQEqmXEESy2ejpBThs4oN6x2VLGlI5LHEf_76AQIlA9YyQ/exec",
     pollMs: 8000,
   },
 };
